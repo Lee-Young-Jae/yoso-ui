94a1117f60f42aa87a8aa77557bf9966a6e7d777
Lee-Young-Jae <ori_@kakao.com>
2024-09-17 22:43:05 +0900

    ✨ feat: ThemeProvider와 Theme를 기본 export에 추가한다.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from "./components/Button/Button";
+export { default as YosoThemeProvider } from "./providers/YosoThemeProvider";
+export { default as defaultTheme } from "./styles/theme";
+export type { Theme } from "./styles/theme";
