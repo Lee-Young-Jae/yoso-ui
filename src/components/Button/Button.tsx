0f5478270e2b4b1c2ca75100ab974a57e04d1e77
Lee-Young-Jae <ori_@kakao.com>
2024-09-18 17:00:59 +0900

    ✨ feat: props에 따른 버튼 스타일을 작성한다.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,197 @@
-import React from "react";
-import styled from "styled-components";
+import { ReactElement, ReactNode } from "react";
+import styled, { css } from "styled-components";
 
-export interface ButtonProps {
-  children: React.ReactNode;
+type Variant = "primary" | "secondary" | "ghost" | "danger" | "default";
+type Size = "xsmall" | "small" | "medium" | "large";
+type Radius = "none" | "small" | "medium" | "large";
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
   onClick?: () => void;
-  variant?: "primary" | "secondary";
+  variant?: Variant;
+  size?: Size;
+  radius?: Radius;
+  outline?: boolean;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  leftIcon?: ReactElement;
+  rightIcon?: ReactElement;
+  color?: string;
 }
 
-const StyledButton = styled.button<{ variant: "primary" | "secondary" }>`
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
+const StyledButton = styled.button<{
+  $variant: ButtonProps["variant"];
+  $size: ButtonProps["size"];
+  $radius: ButtonProps["radius"];
+  $fullWidth: ButtonProps["fullWidth"];
+  $outline: ButtonProps["outline"];
+  $color: ButtonProps["color"];
+}>`
+  // 공통 스타일
+  display: inline-flex;
+  gap: ${({ theme }) => theme.spacing.small};
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: inherit;
+  text-decoration: none;
   cursor: pointer;
-  font-size: ${(props) => props.theme.fontSizes.medium};
-  background-color: ${(props) =>
-    props.variant === "primary"
-      ? props.theme.colors.primary
-      : props.theme.colors.secondary};
-  color: white;
+  user-select: none;
+  border: 1px solid transparent;
+  transition: ${({ theme }) => theme.transitions.default};
+  white-space: nowrap;
+
+  // 크기
+  ${({ $fullWidth }) =>
+    $fullWidth &&
+    css`
+      width: 100%;
+    `}
+
+  // 사이즈
+  ${({ $size, theme }) => {
+    switch ($size) {
+      case "xsmall":
+        return css`
+          font-size: ${theme.fontSizes.small};
+          padding: ${theme.spacing.small} ${theme.spacing.medium};
+        `;
+      case "small":
+        return css`
+          font-size: ${theme.fontSizes.small};
+          padding: ${theme.spacing.medium} ${theme.spacing.large};
+        `;
+      case "large":
+        return css`
+          font-size: ${theme.fontSizes.large};
+          padding: ${theme.spacing.large} ${theme.spacing.xLarge};
+        `;
+      default: // medium
+        return css`
+          font-size: ${theme.fontSizes.medium};
+          padding: ${theme.spacing.medium} ${theme.spacing.large};
+        `;
+    }
+  }}
+
+  // 라디우스
+  ${({ $radius, theme }) => {
+    switch ($radius) {
+      case "none":
+        return css`
+          border-radius: 0;
+        `;
+      case "small":
+        return css`
+          border-radius: ${theme.borderRadius.small};
+        `;
+      case "large":
+        return css`
+          border-radius: ${theme.borderRadius.large};
+        `;
+      default: // medium
+        return css`
+          border-radius: ${theme.borderRadius.medium};
+        `;
+    }
+  }}
+
+  // 색상
+  ${({ $variant, $outline, theme, $color }) => {
+    const getColor = (variant: Variant | undefined) => {
+      if ($color) return $color;
+
+      switch (variant) {
+        case "secondary":
+          return theme.colors.GRAY_500;
+        case "danger":
+          return theme.colors.RED_500;
+        case "ghost":
+        case "default":
+          return theme.defaultColor;
+        default:
+          return theme.defaultColor;
+      }
+    };
+
+    const color = getColor($variant);
+
+    if ($outline) {
+      return css`
+        color: ${color};
+        background-color: transparent;
+        border-color: ${color};
+        &:hover:not(:disabled) {
+          color: white;
+          background-color: ${color};
+        }
+      `;
+    } else if ($variant === "ghost") {
+      return css`
+        color: ${color};
+        background-color: transparent;
+        border-color: transparent;
+        &:hover:not(:disabled) {
+          background-color: ${theme.colors.GRAY_300};
+        }
+      `;
+    } else {
+      return css`
+        color: white;
+        background-color: ${color};
+        border-color: ${color};
+        &:hover:not(:disabled) {
+          background-color: ${theme.colors[
+            `${$variant?.toUpperCase()}_700` as keyof typeof theme.colors
+          ] || color};
+          border-color: ${theme.colors[
+            `${$variant?.toUpperCase()}_700` as keyof typeof theme.colors
+          ] || color};
+        }
+      `;
+    }
+  }}
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.BLUE_100};
+  }
+
+  &:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
 `;
 
-const Button = ({ children, onClick, variant = "primary" }: ButtonProps) => {
+const Button = ({
+  children,
+  onClick,
+  variant = "default",
+  disabled = false,
+  fullWidth = false,
+  size = "medium",
+  radius = "medium",
+  outline = false,
+  leftIcon,
+  rightIcon,
+  color,
+  ...props
+}: ButtonProps) => {
   return (
-    <StyledButton onClick={onClick} variant={variant}>
+    <StyledButton
+      onClick={onClick}
+      $variant={variant}
+      disabled={disabled}
+      $fullWidth={fullWidth}
+      $size={size}
+      $radius={radius}
+      $outline={outline}
+      $color={color}
+      {...props}
+    >
+      {leftIcon && leftIcon}
       {children}
+      {rightIcon && rightIcon}
     </StyledButton>
   );
 };
