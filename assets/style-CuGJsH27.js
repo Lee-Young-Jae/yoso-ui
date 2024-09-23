650eff60389711804c7cddcaaf4da9da73ac0c4f
Lee-Young-Jae <Lee-Young-Jae@users.noreply.github.com>
2024-09-23 13:46:07 +0000

    deploy: 0d19e984ed2f8c9239251dc33bd6af9c64a1b4eb

---
+++ assets/style-CuGJsH27.js
@@ -0,0 +1,1 @@
+function a(t,n){const s=t.replace("#",""),r=parseInt(s.substring(0,2),16),e=parseInt(s.substring(2,4),16),g=parseInt(s.substring(4,6),16);return`rgba(${r}, ${e}, ${g}, ${n})`}export{a as g};
