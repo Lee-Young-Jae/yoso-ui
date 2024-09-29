cd013ca5e0b8c1c1d72e186bbc5505c1735aadb8
Lee-Young-Jae <ori_@kakao.com>
2024-09-29 12:13:18 +0900

    🧑‍💻 feat: children을 조건부로 받을 수 있도록 한다.

--- src/components/Typography/Hightlight/Hightlight.tsx
+++ src/components/Typography/Hightlight/Hightlight.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { CSSProperties, HTMLAttributes, ReactNode } from "react";
+
 import defaultTheme from "../../../styles/theme";
 import { getRGBA } from "../../../utils";
 import { StyledHighlight } from "./Hightlight.styles";
 
 interface HighlightProps extends HTMLAttributes<HTMLSpanElement> {
-  children: ReactNode;
+  children?: ReactNode;
   color?: string;
   offsetY?: number;
   offsetX?: number;
@@ -18,23 +19,29 @@ interface HighlightProps extends HTMLAttributes<HTMLSpanElement> {
 
 const Highlight = ({
   children,
-  color = getRGBA(defaultTheme.defaultColor, 0.3),
+  color,
   offsetX = 0,
   offsetY = 0,
   width = 100,
   height = 50,
   className,
   style,
+  ...props
 }: HighlightProps) => {
   return (
     <StyledHighlight
       style={style}
       className={className}
-      $color={color}
+      $color={
+        typeof color === "string"
+          ? color
+          : getRGBA(defaultTheme.defaultColor, 0.3)
+      }
       $width={width}
       $height={height}
       $offsetX={offsetX}
       $offsetY={offsetY}
+      {...props}
     >
       {children}
     </StyledHighlight>
